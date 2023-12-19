4fe556fcbdd5771b5bccb77b5960a6f3dc086835
unknown <hengya03@gmail.com>
2023-12-18 19:03:46 -0600

    Created a basic nav bar using MUI.

---
+++ vite-project/src/components/MuiNavbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material'
+import WaterDropSharpIcon from '@mui/icons-material/WaterDropSharp';
+
+export default function MuiNavbar() {
+  return (
+    <AppBar postion='static'> 
+        <Toolbar>
+            <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+                <WaterDropSharpIcon/> 
+            </IconButton>
+            <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+                Heng Yang
+            </Typography>
+            <Stack direction='row' spacing={2}>
+                <Button color='secondary' variant='contained'>About Me</Button>
+                <Button color='secondary' variant='contained'>Projects</Button>
+                <Button color='secondary'variant='contained'>Get In Touch</Button>
+            </Stack>
+        </Toolbar>
+    </AppBar>
+  )
+}
